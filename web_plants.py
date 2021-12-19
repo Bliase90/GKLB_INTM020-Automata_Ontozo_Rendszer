b1601a5fb2f0639f886804208fc63e3628e8812f
Balazs G <blaise90@gmail.com>
2021-12-19 19:22:33 +0100

    comments on code added to web_plants.py
    added comments on hungarian language to the code for web_plants.py

--- web_plants.py
+++ web_plants.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, redirect, url_for
-import psutil
-import datetime
-import water
-import os
+from flask import Flask, render_template, redirect, url_for # flask webserver modulok importálása
+import psutil # psutil importálása
+import datetime # date-time importálása
+import water # water.py importálása
+import os # OS modul importálása
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 def template(title = "HELLO!", text = ""):
     now = datetime.datetime.now()
@@ -17,17 +17,17 @@ def template(title = "HELLO!", text = ""):
     return templateDate
 
 @app.route("/")
-def hello():
+def hello(): # töltsbe be a main.html file-t.
     templateData = template()
     return render_template('main.html', **templateData)
 
-@app.route("/last_watered")
+@app.route("/last_watered") # megjeleníti a "get_last_watered" funkció kimeneti értékét
 def check_last_watered():
     templateData = template(text = water.get_last_watered())
     return render_template('main.html', **templateData)
 
 @app.route("/sensor")
-def action():
+def action(): # nedvesség szenzor értéknek megfelelően megjelenített üzenet.
     status = water.get_status()
     message = ""
     if (status == 1):
@@ -39,13 +39,13 @@ def action():
     return render_template('main.html', **templateData)
 
 @app.route("/water")
-def action2():
+def action2(): #ha megfelelő gombra nyomtunk, akkor aktiválja a szivattyút "egyszer"
     water.pump_on()
     templateData = template(text = "Watered Once")
     return render_template('main.html', **templateData)
 
 @app.route("/auto/water/<toggle>")
-def auto_water(toggle):
+def auto_water(toggle): # aktiváljuk az auto_water script-et
     running = False
     if toggle == "ON":
         templateData = template(text = "Auto Watering On")
@@ -64,5 +64,5 @@ def auto_water(toggle):
 
     return render_template('main.html', **templateData)
 
-if __name__ == "__main__":
+if __name__ == "__main__": #konfiguráljuk az IP-t és a port-ot
     app.run(host='0.0.0.0', port=80, debug=True)
